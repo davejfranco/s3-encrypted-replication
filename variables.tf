7c33dd02ee8ffcaa07188781e81f8dce032d200e
Dave Franco <davefranco1987@gmail.com>
2022-01-26 15:37:19 +0100

    bucket names and fmt

--- variables.tf
+++ variables.tf
@@ -4,15 +4,29 @@ variable "src_region" {
 
 variable "dest_region" {
   description = "destination region"
+  type        = string
 }
 
 variable "kms_delete_windows" {
   description = "kms key deletion window in days"
+  type        = number
   default     = 30
 }
 
 variable "s3_acl" {
   description = "s3 bucket access list"
+  type        = string
   default     = "private"
 }
 
+variable "source_bucket_name" {
+  description = "name of the source bucket"
+  type        = string
+  default     = "source"
+}
+
+variable "destination_bucket_name" {
+  description = "name of the destination bucket"
+  type        = string
+  default     = "destination"
+}
